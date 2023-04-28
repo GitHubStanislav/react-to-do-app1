aa95a5a7b540bd2e0b82f86634d25577f3713b3b
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-04-28 15:27:54 +0300

    add todo style

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,9 +1,10 @@
 import { RiTodoFill } from "react-icons/ri";
+import styles from './Todo.module.css'
 function Todo({ todo, index, deleteTodo }) {
   return (
-    <div onDoubleClick={() => deleteTodo(index)}>
-      <div className="flex_div">
-        <RiTodoFill />
+    <div className={styles.todo} onDoubleClick={() => deleteTodo(index)}>
+      <div className={styles.todoText}>
+        <RiTodoFill className={styles.reactIcon} />
         <h3>{todo}</h3>
       </div>
     </div>
--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -1,8 +1,9 @@
 import Todo from "./Todo";
+import styles from "./TodoList.module.css"
 
 function TodoList({ todos, deleteTodo }) {
   return (
-    <div>
+    <div className={styles.todoListContainer}>
       {!todos.length && <h2>Todo list is empty</h2>}
       {todos.map((todo, index) => (
         <Todo key={index} todo={todo} deleteTodo={deleteTodo} index={index} />
