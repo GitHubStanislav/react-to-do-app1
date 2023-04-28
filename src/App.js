74b308ff3ea36cabaaf09a48bb5e8e0d76113f8a
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-04-28 17:03:01 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-
-import {useState} from "react";
-import { v4 as uuidv4 } from 'uuid';
-import './App.css';
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
 import TodoForm from "./components/Todos/TodoForm";
 import TodoList from "./components/Todos/TodoList";
 
-
 function App() {
-  const [todos , setTodos] = useState([])
-  const addTodoHandler = (text) =>{
+  const [todos, setTodos] = useState([]);
+  const addTodoHandler = (text) => {
     const newTodo = {
-        text: text,
-        isCompleted: false,
-        id: uuidv4()
-    }
-      setTodos([...todos, newTodo])
-  }
-  const deleteTodoHandler = (index)=>{
-    setTodos(todos.filter((_,idx) => idx !==index))
-  }
+      text: text,
+      isCompleted: false,
+      id: uuidv4(),
+    };
+    setTodos([...todos, newTodo]);
+  };
+  const deleteTodoHandler = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
   return (
-      <div className="App">
-       <h1>Todo App 1</h1>
-        <TodoForm addTodo ={addTodoHandler}/>
-        <TodoList todos={todos} deleteTodo={deleteTodoHandler}/>
-      </div>
+    <div className="App">
+      <h1>Todo App 1</h1>
+      <TodoForm addTodo={addTodoHandler} />
+      <TodoList todos={todos} deleteTodo={deleteTodoHandler} />
+    </div>
   );
 }
 
--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,11 +1,11 @@
 import { RiTodoFill } from "react-icons/ri";
 import styles from './Todo.module.css'
-function Todo({ todo, index, deleteTodo }) {
+function Todo({ todo, deleteTodo }) {
   return (
-    <div className={styles.todo} onDoubleClick={() => deleteTodo(index)}>
+    <div className={styles.todo} onDoubleClick={() => deleteTodo(todo.id)}>
       <div className={styles.todoText}>
         <RiTodoFill className={styles.reactIcon} />
-        <h3>{todo}</h3>
+        <h3>{todo.text}</h3>
       </div>
     </div>
   );
