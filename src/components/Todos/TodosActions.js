6793f413899d396d280213843e22062ca1e6cb0f
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-04-29 18:00:28 +0300

    +

--- src/components/Todos/TodosActions.js
+++ src/components/Todos/TodosActions.js
@@ -1,16 +1,17 @@
 import { RiDeleteBin2Line, RiRefreshLine } from "react-icons/ri";
 import Button from "../UI/Button";
+import styles from './Todos.Actions.module.css'
 
 function TodosActions({ resetTodos, deleteCompletedTodos, completedTodosExist }) {
   return (
-    <>
+    <div className={styles.todosActionsContainer}>
       <Button title="Reset Todos" onClick={resetTodos}>
         <RiRefreshLine />
       </Button>
       <Button disabled={!completedTodosExist} title="Clear Complited" onClick={deleteCompletedTodos}>
         <RiDeleteBin2Line />
       </Button>
-    </>
+    </div>
   );
 }
 
