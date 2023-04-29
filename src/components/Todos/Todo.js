c971e8aad50399529048123ac8dff1951f5ef247
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-04-29 12:41:25 +0300

    add styles buttons

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,11 +1,17 @@
-import { RiTodoFill } from "react-icons/ri";
-import styles from './Todo.module.css'
+import { RiTodoFill, RiDeleteBin2Line } from "react-icons/ri";
+import { FaCheck } from "react-icons/fa";
+import styles from "./Todo.module.css";
 function Todo({ todo, deleteTodo }) {
   return (
     <div className={styles.todo} onDoubleClick={() => deleteTodo(todo.id)}>
       <div className={styles.todoText}>
-        <RiTodoFill className={styles.reactIcon} />
+        <RiTodoFill className={styles.todoIcon} />
         <h3>{todo.text}</h3>
+        <div>
+            <RiDeleteBin2Line className={styles.deleteIcon} />
+            <FaCheck className={styles.checkIcon}/>
+        </div>
+
       </div>
     </div>
   );
